78563a2e632d5f77f7cb5fbdf5bd12e4f468867a
bitsima <betulse42@gmail.com>
2023-07-15 00:08:42 +0300

    feat: add module database.py, define database schema

---
+++ tests/test_database.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import pytest
+
+from ..src import database
+from ..src import utils
+
+
+@pytest.fixture
+def database(scope="function"):
+    # setup
+    credentials = utils.read_credentials()
+
+    engine = create_engine(
+        f'postgresql+psycopg2://{credentials[0]}:{credentials[1]}@localhost:5432/database')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    yield session
+
+    # teardown
+    session.close()
+    engine.dispose()
