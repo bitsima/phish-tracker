99f4799505ce383f021e9d6111620786d4f9f109
bitsima <betulse42@gmail.com>
2023-07-17 18:42:50 +0300

    style: formatting

--- src/database/database.py
+++ src/database/database.py
@@ -6,11 +6,10 @@ from utils import read_credentials
 
 
 credentials = read_credentials()
-DATABASE_URL = (
-    f"postgresql+psycopg2://{credentials[0]}:{credentials[1]}@localhost:5432/database"
-)
+DATABASE_URL = f"postgresql+psycopg2://{credentials[0]}:{credentials[1]}@localhost:5432/fastapi_database"
+
 engine = create_engine(DATABASE_URL)
 
-Session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
