d55e9bd02a2c84b0e0f3799ae7aea0d17db42b85
bitsima <betulse42@gmail.com>
2023-07-17 18:47:22 +0300

    feat: add services module

--- src/database/services.py
+++ src/database/services.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Enum, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import orm
 
-from . import _database
+import time
+
+from . import database as _database
 from . import models
+from api import schemas
 
 
 def add_tables():
@@ -10,31 +12,60 @@ def add_tables():
     return database
 
 
-def add_to_db(
-    PhishTank_id: Integer,
-    url: String,
-    status: Enum,
-    is_online: Enum,
-    added_at: String,
-    description: String,
-    submitted_by: String,
-) -> None:
-    session = sessionmaker.Session()
+def get_db():
+    db = _database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-    new_site = models.PhishingSite(
-        PhishTank_id=PhishTank_id,
-        url=url,
-        status=status,
-        is_online=is_online,
-        added_at=added_at,
-        description=description,
-        submitted_by=submitted_by,
+
+async def create_site(
+    site: schemas.PhishingSiteCreate, db: orm.Session
+) -> schemas.PhishingSite:
+    site = models.PhishingSite(**site.model_dump())
+    db.add(site)
+    db.commit()
+    db.refresh(site)
+
+    return schemas.PhishingSite(**site.__dict__)
+
+
+async def get_last_saved(db: orm.Session):
+    last_saved = (
+        db.query(models.PhishingSite)
+        .order_by(models.PhishingSite.submitted_at.desc())
+        .first()
     )
-
-    session.add(new_site)
-    session.commit()
-    session.close()
+    return last_saved
 
 
-def remove_from_db(PhishTank_id: int) -> None:
-    pass
+async def get_all_sites(db: orm.Session) -> list[schemas.PhishingSite]:
+    sites = db.query(models.PhishingSite).all()
+    return list(map(schemas.PhishingSite.from_orm, sites))
+
+
+async def get_site(PhishTank_id: int, db: orm.session) -> schemas.PhishingSite:
+    site = (
+        db.query(models.PhishingSite)
+        .filter(models.PhishingSite.PhishTank_id == PhishTank_id)
+        .first()
+    )
+    return site
+
+
+async def update_site(
+    site_data: schemas.PhishingSiteCreate, site: models.PhishingSite, db: orm.Session
+) -> schemas.PhishingSite:
+    site.status = site_data.status
+    site.is_online = site_data.is_online
+    site.description = site_data.description
+
+    db.commit()
+    db.refresh(site)
+    return schemas.PhishingSite(**site.__dict__)
+
+
+async def delete_site(site: models.PhishingSite, db: orm.Session) -> None:
+    db.delete(site)
+    db.commit()
