9ca5a96f976d482875f900c930c0e2082fb6f55e
bitsima <betulse42@gmail.com>
2023-07-17 18:45:30 +0300

    feat: add get_timestamp functionality and remove regex from read_credentials

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,66 @@
-import re
+from datetime import datetime
 
 
 def read_credentials() -> list[str]:
-    '''Reads the database credentials from the config.ini file and returns them as a list{username, password}.'''
-    with open('config.ini') as f:
+    """Reads the database credentials from the config.ini file and returns them as a list{username, password}."""
+    with open("config.ini") as f:
         credentials = f.read()
 
-    username_match = re.search(
-        r"^(username)[ ]*=(?=\s*(\w+))", credentials, re.IGNORECASE)
-    if username_match:
-        username = username_match.group(2)
-
-    password_match = re.search(
-        r"^(password)[ ]*=(?=\s*(\w+))", credentials, re.IGNORECASE)
-    if password_match:
-        password = password_match.group(2)
+    credentials = credentials.split("\n")[1:]
+    credentials = [x.split(" ")[-1] for x in credentials]
+    username = credentials[0]
+    password = credentials[1]
 
     return [username, password]
+
+
+def get_timestamp(date: str) -> str:
+    date_lst = date.split(" ")
+    month_name = date_lst[2]
+    day_num = date_lst[3]
+    year = date_lst[4]
+    hour_12h = date_lst[5]
+    flag = date_lst[6]
+
+    month = ""
+    match month_name:
+        case "Jan":
+            month = "1"
+        case "Feb":
+            month = "2"
+        case "Mar":
+            month = "3"
+        case "Apr":
+            month = "4"
+        case "May":
+            month = "5"
+        case "Jun":
+            month = "6"
+        case "Jul":
+            month = "7"
+        case "Aug":
+            month = "8"
+        case "Sep":
+            month = "9"
+        case "Oct":
+            month = "10"
+        case "Nov":
+            month = "11"
+        case "Dec":
+            month = "12"
+
+    day = ""
+    if day_num[:2].isnumeric():
+        day = day_num[:2]
+    else:
+        day = day_num[0]
+
+    if flag == "PM":
+        hour = int(hour_12h.split(":")[0]) + 12 % 24
+    else:
+        hour = int(hour_12h.split(":")[0])
+
+    minutes = int(hour_12h.split(":")[1])
+    dtime = datetime(int(year), int(month), int(day), hour, minutes)
+
+    return str(dtime.timestamp())
