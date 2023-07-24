53d5a28473c18063ea16895f882803cc32f39132
bitsima <betulse42@gmail.com>
2023-07-24 14:11:29 +0300

    feat: add scraping support for all types of submissions

--- src/scraper.py
+++ src/scraper.py
@@ -3,12 +3,13 @@ from getuseragent import UserAgent
 import requests
 
 import time
-import datetime
 import logging
 
 from .api import schemas
 from . import logging_config
 from . import utils
+from .database import services
+
 
 # configuring the Logger object for the module
 logging_config.configure_logging()
@@ -17,7 +18,7 @@ logger = logging.getLogger(__name__)
 soup = ""
 
 
-def get_main_page(verified="y", active="y") -> int:
+def get_main_page() -> int:
     submission = None
     while submission == None:
         global soup
@@ -28,7 +29,7 @@ def get_main_page(verified="y", active="y") -> int:
 
         headers = {"User-agent": the_user_agent}
 
-        url = f"https://phishtank.org/phish_search.php?verified={verified}&active={active}"
+        url = "https://phishtank.org/phish_archive.php"
 
         response = requests.get(url, headers=headers)
 
@@ -42,13 +43,49 @@ def get_main_page(verified="y", active="y") -> int:
         soup = BeautifulSoup(html_text, "lxml")
 
         submission = soup.find("tr", style="background: #ffffcc;")
+        if submission == None:
+            submission = soup.find("tr", style="background: #ffffff;")
 
     return 0
 
 
-def get_submissions(verified="y") -> list[schemas.PhishingSiteCreate]:
-    if verified == "y":
-        submissions = soup.find_all("tr", style="background: #ffffcc;")
+def get_updated_data(PhishTank_id: int) -> int:
+    user_agent = UserAgent()  # instantiating UserAgent seed
+
+    the_user_agent = user_agent.Random()  # Generating a fake randomized user agent
+
+    headers = {"User-agent": the_user_agent}
+
+    url = f"https://phishtank.org/phish_detail.php?phish_id={PhishTank_id}"
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        logger.exception(f"Html error occurred. Status code: {response.status_code}")
+        return 1
+
+    html_text = response.text
+    soup_submission = BeautifulSoup(html_text, "lxml")
+
+    raw_status = soup_submission.find("tr").find_all(recursive=False)[1].find("h3").text
+
+    raw_is_online = (
+        soup_submission.find("h2", style="margin:0;border:0;")
+        .find("span", style="font-weight:normal;")
+        .text
+    )
+
+    status, is_online = utils.status_formatter(raw_status, raw_is_online)
+
+    site_schema = schemas.PhishingSiteUpdate(
+        status=status, is_online=is_online, description=""
+    )
+
+    return site_schema
+
+
+def get_submissions() -> list[schemas.PhishingSiteCreate]:
+    submissions = soup.find("table", class_="data").find_all(recursive=False)[1:-5]
 
     site_list = []
     for submission in submissions:
@@ -58,23 +95,15 @@ def get_submissions(verified="y") -> list[schemas.PhishingSiteCreate]:
     return site_list
 
 
-def get_site_object(
-    submission: BeautifulSoup, verified="y", active="y"
-) -> schemas.PhishingSiteCreate:
+def get_site_object(submission: BeautifulSoup) -> schemas.PhishingSiteCreate:
     values = submission.find_all("td", class_="value")
 
     Phishtank_id = int(values[0].find("a").text)
     url = values[1].find_next(string=True).strip()
 
-    if verified == "y":
-        status = "Valid"
-    else:
-        status = schemas.StatusEnum.SUSPECTED
+    status = values[3].text
 
-    if active == "y":
-        is_online = "True"
-    else:
-        is_online = schemas.IsOnlineEnum.FALSE
+    is_online = values[4].text
 
     # converting into timestamp
     submitted_at = utils.get_timestamp(values[1].find("span").text)
@@ -93,13 +122,3 @@ def get_site_object(
     )
 
     return site
-
-
-"""
-
-
-
-https://phishtank.org/phish_search.php?verified=u&active=y
-https://phishtank.org/phish_search.php?verified=y&active=y
-https://phishtank.org/phish_search.php?verified=y&active=n
-https://phishtank.org/phish_search.php?verified=u&active=n """
