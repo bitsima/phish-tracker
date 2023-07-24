7f2ae4b9f1a9c9ada59129c9222fc4c6d5b65805
bitsima <betulse42@gmail.com>
2023-07-24 14:15:45 +0300

    fix: add update messages and change update schema

--- src/database/services.py
+++ src/database/services.py
@@ -1,6 +1,5 @@
 from sqlalchemy import orm
 
-
 from . import database as _database
 from . import models
 from ..api import schemas
@@ -44,7 +43,7 @@ async def get_all_sites(db: orm.Session) -> list[schemas.PhishingSite]:
     return list(map(schemas.PhishingSite.from_orm, sites))
 
 
-async def get_site(PhishTank_id: int, db: orm.session) -> schemas.PhishingSite:
+async def get_site(PhishTank_id: int, db: orm.session) -> models.PhishingSite:
     site = (
         db.query(models.PhishingSite)
         .filter(models.PhishingSite.PhishTank_id == PhishTank_id)
@@ -54,15 +53,31 @@ async def get_site(PhishTank_id: int, db: orm.session) -> schemas.PhishingSite:
 
 
 async def update_site(
-    site_data: schemas.PhishingSiteCreate, site: models.PhishingSite, db: orm.Session
+    site_data: schemas.PhishingSiteUpdate, site: models.PhishingSite, db: orm.Session
 ) -> schemas.PhishingSite:
+    update_message = dict()
+    if site.status != site_data.status:
+        update_message[
+            "message_status"
+        ] = f"INFO: Updated site with id {site.PhishTank_id}, changed previous status - {site.status} to {site_data.status}"
     site.status = site_data.status
+
+    if site.is_online != site_data.is_online:
+        update_message[
+            "message_online"
+        ] = f"INFO: Updated site with id {site.PhishTank_id}, changed previous is_online - {site.is_online} to {site_data.is_online}"
+
     site.is_online = site_data.is_online
+
+    if site.description != site_data.description:
+        update_message[
+            "message_desc"
+        ] = f"INFO: Updated site with id {site.PhishTank_id}, changed previous description - {site.description} to {site_data.description}"
     site.description = site_data.description
 
     db.commit()
     db.refresh(site)
-    return schemas.PhishingSite(**site.__dict__)
+    return update_message
 
 
 async def delete_site(site: models.PhishingSite, db: orm.Session) -> None:
